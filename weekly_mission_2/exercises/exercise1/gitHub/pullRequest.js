5f71e479e774c0510eb742adc2350fa4f2f7b58c
Cristian Flores <cris18002011@hotmail.com>
2022-04-20 13:27:16 -0500

    W.2 ejercicio1 pull request

---
+++ weekly_mission_2/exercises/exercise1/gitHub/pullRequest.js
@@ -0,0 +1,16 @@
+const pullRequest = {
+    title: "Agregue mi blog",
+    branchName: "Main",
+    repositoryNameAssociated: "myBlog-LaunchX",
+    status: "Open",
+    author: "elchicogamer117",
+    getStatus: function(){
+        return `Status: ${this.status}` 
+    },
+    getTitleAndAuthor: function(){
+        return `Title: ${this.title} | Author: ${this.author}` 
+  }
+}
+
+console.log("Nombre y autor del Pull requests:" + pullRequest.getTitleAndAuthor())
+console.log(pullRequest.getStatus())
