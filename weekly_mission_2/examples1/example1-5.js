1c148233320a916af2b6068546c8f565dbaaed4e
Cristian <cris18002011@hotmail.com>
2022-04-20 19:59:57 -0500

    W2. Modificación ejemplo 1-5

--- weekly_mission_2/examples1/example1-5.js
+++ weekly_mission_2/examples1/example1-5.js
@@ -1,6 +1,10 @@
-// Ejemplo 5: Uso de Map para convertir todos los nombres de una lista a mayúscula
+// Ejemplo 5: Uso de Map para convertir todos los nombres de una lista a mayúscula y/o minusculas
+
 const names = ['Jonh', 'Cortana', 'Halsey', 'Jonhson', 'Fernando Esparza']
 const namesToUpperCase = names.map((name) => name.toUpperCase())
+const namesToLowerCase = names.map((name) => name.toLowerCase())
 
 console.log("Ejemplo 5: Array de nombres en mayúscula:")
 console.log(namesToUpperCase)
+console.log("Array de nombres en minúscula:")
+console.log(namesToLowerCase)
