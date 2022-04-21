d4fe15c618fb9b2424186f22b7f7bdae4d9d3a5b
Cristian <cris18002011@hotmail.com>
2022-04-20 19:41:47 -0500

    W2. Prueba ejemplo 1-5

---
+++ weekly_mission_2/examples1/example1-5.js
@@ -0,0 +1,6 @@
+// Ejemplo 5: Uso de Map para convertir todos los nombres de una lista a mayúscula
+const names = ['Jonh', 'Cortana', 'Halsey', 'Jonhson', 'Fernando Esparza']
+const namesToUpperCase = names.map((name) => name.toUpperCase())
+
+console.log("Ejemplo 5: Array de nombres en mayúscula:")
+console.log(namesToUpperCase)
--- weekly_mission_2/exercises/exercise1/gitHub/pullRequest.js
+++ weekly_mission_2/exercises/exercise1/gitHub/pullRequest.js
@@ -1,15 +1,15 @@
 const pullRequest = {
-    title: "Agregue mi blog",
-    branchName: "Main",
-    repositoryNameAssociated: "myBlog-LaunchX",
-    status: "Open",
-    author: "elchicogamer117",
-    getStatus: function(){
-        return `Status: ${this.status}` 
-    },
-    getTitleAndAuthor: function(){
-        return `Title: ${this.title} | Author: ${this.author}` 
-  }
+      title: "Agregue mi blog",
+      branchName: "Main",
+      repositoryNameAssociated: "myBlog-LaunchX",
+      status: "Open",
+      author: "elchicogamer117",
+      getStatus: function(){
+            return `Status: ${this.status}` 
+      },
+      getTitleAndAuthor: function(){
+            return `Title: ${this.title} | Author: ${this.author}` 
+      }
 }
 
 console.log("Nombre y autor del Pull requests:" + pullRequest.getTitleAndAuthor())
