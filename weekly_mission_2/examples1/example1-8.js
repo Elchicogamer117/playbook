57ad8fcdc7c9a2611560c517bbee83cf0daeba09
Cristian <cris18002011@hotmail.com>
2022-04-20 20:34:29 -0500

    W2. Prueba ejemplo1-8

---
+++ weekly_mission_2/examples1/example1-8.js
@@ -0,0 +1,18 @@
+// Ejemplo 8: Filtrar una lista por condicional
+const spartans = [
+      { name: 'John', ID: 117 },
+      { name: 'Kai', ID: 125 },
+      { name: 'Vannak', ID: 134 },
+      { name: 'Riz', ID: 28 },
+      { name: 'Fred', ID: 104 },
+      { name: 'Kelly', ID: 87 },
+      ]
+      
+const maxSpartanID = spartans.filter((spartan) => spartan.ID > 104)
+      console.log("Filtrando spartans con ID > 104:")
+      console.log(maxSpartanID)
+
+const minSpartanID = spartans.filter((spartan) => spartan.ID <= 104)
+      console.log("Filtrando spartans con ID < 104:")
+      console.log(minSpartanID)
+      
