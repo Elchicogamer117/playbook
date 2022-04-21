92dc2bb7a3e3f4232dca1976273e2e58e9708bea
Cristian <cris18002011@hotmail.com>
2022-04-20 21:31:03 -0500

    W2. Prueba ejemplo1-15

---
+++ weekly_mission_2/examples1/example1-15.js
@@ -0,0 +1,5 @@
+//Ejemplo 15: Uso de sort para ordenar elementos
+
+const products = ['Milk', 'Coffee', 'Sugar', 'Honey', 'Apple', 'Carrot', 'banana', '1 Orange']
+console.log("Elementos ordernados de acuerdo a su valor Unicode:") //* <-  Las mayúsculas y números tienen prioridad
+console.log(products.sort())
