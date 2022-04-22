5d3a5c1e409be9064b156223935ab591336e3cea
Cristian <cris18002011@hotmail.com>
2022-04-22 18:27:31 -0500

    W2. Prueba ejercicio2 - Puntos 5-8

--- weekly_mission_2/exercises/exercise2/explorers.js
+++ weekly_mission_2/exercises/exercise2/explorers.js
@@ -1,8 +1,8 @@
 const explorers = [
       {
-            name: "John 117",
-            exercises_completed: 10,
-            rate: 99,
+            name: "John",
+            exercises_completed: 117,
+            rate: 343,
             city: "CDMX",
             stack: [
                   "js",
@@ -40,7 +40,7 @@ const explorers = [
       },
       {
             name: "Fernando Esparza",
-            exercises_completed: 3,
+            exercises_completed: 4,
             city: "Sonora",
             rate: 100,
             stack: [
@@ -53,7 +53,7 @@ const explorers = [
             },
             frontend: {
                   isFinished: false,
-                  exercisesFinished: false
+                  exercisesFinished: true
             }
             }
       },
@@ -81,19 +81,33 @@ const explorers = [
       }
 ]
 
-// console.log('1.Nombre de cada explorer en la lista:')
-// explorers.forEach((element) => console.log(element.name))
+console.log('1.Nombre de cada explorer en la lista:')
+explorers.forEach((element) => console.log(element.name))
 
-// console.log('2.Stack de cada explore:')
-// explorers.forEach((explorer) => console.log(explorer.stack))
+console.log('2.Stack de cada explorer:')
+explorers.forEach((explorer) => console.log(explorer.stack))
 
-// console.log('3.Nueva lista con las stacks de cada explorer:')
-// const stackExplorer = explorers.map((explorer) =>  {return explorer.stack} )
-// console.log (stackExplorer)
-
-
-const countries = ['Finland', 'Denmark', 'Sri lanka', 'Sweden', 'Norway', 'Iceland', 'Portugal', 'Netherlands', 'Tanzania']  
+console.log('3.Nueva lista con las stacks de cada explorer:')
+const stackExplorer = explorers.map((explorer) =>  {return explorer.stack} )
+console.log (stackExplorer)
 
 console.log('4.Lista de explorers que tengan en su stack "js":')
 const stackJs = explorers.filter((explorer) => explorer.stack.includes('js'))
 console.log(stackJs)
+
+console.log('5.Primer explorer que sea de la CDMX:')
+const explorerCDMX = explorers.find((explorer) => explorer.city = 'CDMX')
+console.log(explorerCDMX)
+
+console.log('6.Suma de todos los exercises_completed:')
+const totalExercises = explorers.reduce((explorerM, explorerN) => explorerM + explorerN.exercises_completed, 0)
+console.log(totalExercises)
+
+console.log('7.Validación - Al menos uno de los explorers tiene la propiedad exercisesFinished en frontend como true:')
+const frontExFin = explorers.some((explorer) => explorer.missions.frontend.exercisesFinished === true) //*? <- Funciona igual con "==" dado que si pongo" exercisesFinished: 'true' " lo toma como false
+console.log(frontExFin)
+
+console.log('8.Validación - Todos los explorers tienen la propiedad isFinished del onboarding como true:')
+const allOnbordFin = explorers.every((explorer) => explorer.missions.onboarding.isFinished === true) //*? <- Funciona igual con "==" dado que si pongo" isFinished: 'true' " lo toma como false
+console.log(allOnbordFin)
+
