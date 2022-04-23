578eff23044d3612e534d4b2e84f3081431b1fa2
Cristian <cris18002011@hotmail.com>
2022-04-23 13:31:00 -0500

    W2. Modificación ejercicio3

--- weekly_mission_2/exercises/exercise3/main.js
+++ weekly_mission_2/exercises/exercise3/main.js
@@ -1,4 +1,19 @@
 import Repositorio from "./repo.js"
 
-const playbook = new Repositorio('playBook', 'elchicogammer','JavaScript', 18, 2, 1, 5, 2)
+//* instanciando un objeto con todos los atributos
+const playbook = new Repositorio('playBook', 'Elchicogamer117','JavaScript', 18, 2, 1, 5, 2) 
 console.log(playbook)
+console.log(playbook.getGeneralInfo())
+console.log(`Issues Totales: ${playbook.getTotalIssues()}`)
+
+//*instanciando un objeto por default
+const launchX = new Repositorio()
+console.log(launchX)
+console.log(launchX.getGeneralInfo())
+console.log(`Total de issues: ${launchX.getTotalIssues()}`)
+
+//*instanciando un objeto con algunos atributos vacios 
+const pokedex = new Repositorio('Pokedex','Elchicogamer','', 2, 1, 0,'0' , "",) //*? <- el valor 0 lo toma como vacio y toma el valor by default
+console.log(pokedex)
+console.log(`Información general: ${pokedex.getGeneralInfo()} | Issues: ${pokedex.getTotalIssues()}`)
+
